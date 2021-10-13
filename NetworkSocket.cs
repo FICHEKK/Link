695a19a57a8f6cf212baa5fb355800866ea2a7aa
FICHEKK <filipnemec3@gmail.com>
2021-10-13 16:55:32 +0200

    Make network socket a non Monobehaviour class. Add packet loss and latency fields to network node.

--- NetworkSocket.cs
+++ NetworkSocket.cs
@@ -1,22 +1,38 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 using UnityEngine;
+using Random = System.Random;
 
 namespace Networking.Transport
 {
-    public class NetworkSocket : MonoBehaviour
+    public class NetworkSocket : IDisposable
     {
         private static readonly EndPoint AnyEndPoint = new IPEndPoint(IPAddress.Any, 0);
 
         private readonly byte[] _receiveBuffer = new byte[1024];
-        private Socket _socket;
-        private IPacketHandler _packetHandler;
+        private readonly Socket _socket;
+        private readonly Random _random = new Random();
+        private readonly IPacketHandler _packetHandler;
 
-        public void Listen(Socket socket, IPacketHandler packetHandler)
+        public float PacketLossProbability { get; set; }
+        public int Latency { get; set; }
+
+        public NetworkSocket(Socket socket, IPacketHandler packetHandler, float packetLossProbability = 0, int latency = 0)
         {
+            if (packetLossProbability < 0 || packetLossProbability > 1)
+                throw new ArgumentException($"Packet loss probability must be a value in range from 0 to 1. Provided value: {packetLossProbability}");
+
+            if (latency < 0)
+                throw new ArgumentException($"Latency must be a positive value. Provided value: {latency}");
+
+            PacketLossProbability = packetLossProbability;
+            Latency = latency;
+
             _socket = socket ?? throw new NullReferenceException(nameof(socket));
             _packetHandler = packetHandler ?? throw new NullReferenceException(nameof(packetHandler));
+
             ReceiveFromAnySource();
         }
 
@@ -57,13 +73,23 @@ namespace Networking.Transport
             }
         }
 
-        protected virtual void HandlePacket(Packet packet, EndPoint senderEndPoint) =>
+        private async void HandlePacket(Packet packet, EndPoint senderEndPoint)
+        {
+            if (PacketLossProbability > 0 && _random.NextDouble() < PacketLossProbability)
+            {
+                Debug.Log($"Packet of type {packet.Type} was lost.");
+                return;
+            }
+
+            if (Latency > 0) await Task.Delay(Latency);
+
             _packetHandler.Handle(packet, senderEndPoint);
+        }
 
         public void Send(Packet packet, EndPoint receiverEndPoint) =>
             packet.Send(_socket, receiverEndPoint);
 
-        private void OnDisable() =>
+        public void Dispose() =>
             _socket.Dispose();
     }
 }
