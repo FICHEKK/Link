b7bf739249854ab79d74b6fb8cf0ad280d9760b6
FICHEKK <filipnemec3@gmail.com>
2022-03-07 17:43:27 +0100

    Fully decouple "Transport" namespace from any Unity or higher level networking library components, which makes it reusable in any environment.

--- PacketTypeExtensions.cs
+++
@@ -1,8 +0,0 @@
-namespace Networking.Transport
-{
-    public static class PacketTypeExtensions
-    {
-        public static ushort Id(this PacketType packetType) =>
-            (ushort) packetType;
-    }
-}
