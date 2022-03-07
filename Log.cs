b1ad83e2ae337fc46bc54114d25959372db4d055
FICHEKK <filipnemec3@gmail.com>
2022-03-07 15:11:21 +0100

    Decouple transport layer logger from "UnityEngine" namespace.

--- Log.cs
+++ Log.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+using System;
 
 namespace Networking.Transport
 {
@@ -12,19 +12,35 @@ namespace Networking.Transport
         /// </summary>
         public delegate void Logger(string message);
 
+        private static Logger _info;
+        private static Logger _warning;
+        private static Logger _error;
+
         /// <summary>
         /// Logs informative messages that are useful during development.
         /// </summary>
-        public static Logger Info = Debug.Log;
+        public static Logger Info
+        {
+            get => _info ?? throw new NullReferenceException($"{nameof(Info)} logger hasn't been initialized.");
+            set => _info = value ?? throw new NullReferenceException($"{nameof(Info)} logger cannot be set to null.");
+        }
 
         /// <summary>
         /// Logs unexpected events that might indicate a problem in application logic.
         /// </summary>
-        public static Logger Warning = Debug.LogWarning;
+        public static Logger Warning
+        {
+            get => _warning ?? throw new NullReferenceException($"{nameof(Warning)} logger hasn't been initialized.");
+            set => _warning = value ?? throw new NullReferenceException($"{nameof(Warning)} logger cannot be set to null.");
+        }
 
         /// <summary>
         /// Logs serious problems that should never occur during application runtime.
         /// </summary>
-        public static Logger Error = Debug.LogError;
+        public static Logger Error
+        {
+            get => _error ?? throw new NullReferenceException($"{nameof(Error)} logger hasn't been initialized.");
+            set => _error = value ?? throw new NullReferenceException($"{nameof(Error)} logger cannot be set to null.");
+        }
     }
 }
